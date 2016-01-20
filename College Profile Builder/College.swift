3f1e16bb7f0b2235f9754231e7b8a256a1dddb86
Pepper O'Brien <eightyob@comcast.net>
2016-01-20 11:53:35 -0600

    Set up storyboard and created custom classes

---
+++ College Profile Builder/College.swift
@@ -0,0 +1,13 @@
+//
+//  College.swift
+//  College Profile Builder
+//
+//  Created by Student on 1/20/16.
+//  Copyright © 2016 George O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class College: NSObject {
+
+}
